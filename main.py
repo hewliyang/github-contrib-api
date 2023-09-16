6113bfaa14d65f99f48796df0e8573f5ff786a31
hewliyang <hewliyang@u.nus.edu>
2023-09-17 00:06:17 +0800

    bug: remove middleware?

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-import uvicorn
-
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from github import Client, ContributionsRequestParams
 
 client = Client()
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 @app.get("/", include_in_schema=False)
@@ -25,7 +14,3 @@ async def root():
 @app.post("/contributions")
 async def fetch_contributions(params: ContributionsRequestParams):
     return client.fetch_contributions(params)
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
